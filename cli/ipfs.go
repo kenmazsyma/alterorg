ca0926de8912cbd41cd3f0d039a3d7835a1b9e42
Ken Matsushima <ken.ma24ma@yahoo.com>
2016-06-17 00:24:25 +0900

    proofread ipfs.go

--- cli/ipfs.go
+++ cli/ipfs.go
@@ -12,10 +12,20 @@ import (
 	"os"
 )
 
+type ErrCodeIpfs string
+
 var ipfsurl string
 var shell *ipfs.Shell
 var myid string
 var nsAdrs string
+
+func makeError(msg ErrCodeIpfs) error {
+	return errors.New(string(msg))
+}
+
+func IsEqualErr(err error, tp ErrCodeIpfs) bool {
+	return err.Error() == string(tp)
+}
 
 func InitIpfs(url string) error {
 	ipfsurl = url
@@ -30,42 +40,38 @@ func InitIpfs(url string) error {
 }
 
 func IpfsAddFile(path string) (string, error) {
-	file, er := os.Open(path)
-	if er != nil {
-		return "", er
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
 	}
 	return IpfsAdd(file)
 }
 
 func IpfsAdd(reader io.Reader) (string, error) {
-	hash, er := shell.Add(reader)
-	if er != nil {
-		return "", er
+	hash, err := shell.Add(reader)
+	if err != nil {
+		return "", err
 	}
 	return hash, nil
-}
-
-func IpfsGet(hash, path string) error {
-	return shell.Get(hash, path)
-}
-
-func IpfsBlockGet(hash string) ([]byte, error) {
-	return shell.BlockGet(hash)
 }
 
 var DIR_IPFS_BOARD = "boardforalterorg"
 var dIR_IPFS = "/ipfs/"
 var hASH_EMPTY_FILE = "QmbFMke1KXqnYyBBWxB74N4c5SBnJMVAiMNRcGu6x1AwQH"
 var hASH_EMPTY_DIR = "QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn"
-var ERR_IpfsCreateIpfsDir_01 = "01" // ipns link is created as a file
-var ERR_IpfsCreateIpfsDir_02 = "02" // dir which is used for boards of alterorg is created as a file
+
+const (
+	ERR_IpfsCreateIpfsDir_01 ErrCodeIpfs = "01" // ipns link is created as a file
+	ERR_IpfsCreateIpfsDir_02 ErrCodeIpfs = "02" // dir which is used for boards of alterorg is created as a file
+)
+
 func IpfsCreateBoardDir() error {
 	obj, err := shell.ObjectGet(nsAdrs)
 	if err != nil {
 		return err
 	}
 	if len(obj.Links) == 0 && nsAdrs != dIR_IPFS+hASH_EMPTY_DIR { // link to a file
-		return errors.New(ERR_IpfsCreateIpfsDir_01)
+		return makeError(ERR_IpfsCreateIpfsDir_01)
 	} else { // link to a directory
 		lst, err := shell.List(nsAdrs)
 		if err != nil {
@@ -75,7 +81,7 @@ func IpfsCreateBoardDir() error {
 		for _, v := range lst {
 			if v.Name == DIR_IPFS_BOARD {
 				if v.Type == 2 { // 1:dir 2:file
-					return errors.New(ERR_IpfsCreateIpfsDir_02)
+					return makeError(ERR_IpfsCreateIpfsDir_02)
 				}
 				nodir = false
 				break
@@ -90,15 +96,11 @@ func IpfsCreateBoardDir() error {
 				return err
 			}
 			obj.Data = string(buf)
-			if err != nil {
-				return err
-			}
 			newhash, err := shell.ObjectPut(obj)
 			if err != nil {
 				return err
 			}
-			putIpnsAdrs(newhash)
-			if err != nil {
+			if err = putIpnsAdrs(newhash); err != nil {
 				return err
 			}
 		}
@@ -125,8 +127,11 @@ func putIpnsAdrs(adrs string) error {
 	return nil
 }
 
-var ERR_IpfsWriteToBoard_01 = "01" // dir for boards is not created yet
-var ERR_IpfsWriteToBoard_02 = "02" // failed to get dir for boards
+const (
+	ERR_IpfsWriteToBoard_01 ErrCodeIpfs = "01" // dir for boards is not created yet
+	ERR_IpfsWriteToBoard_02 ErrCodeIpfs = "02" // failed to get dir for boards
+)
+
 func IpfsWriteToBoard(data string, n string) error {
 	size := uint64(len(data))
 	buf, err := proto.Marshal(&pb.Data{Type: pb.Data_File.Enum(), Data: []byte(data), Filesize: &size})
@@ -143,7 +148,7 @@ func IpfsWriteToBoard(data string, n string) error {
 	obj, err := shell.ObjectGet(nsAdrs + "/" + DIR_IPFS_BOARD)
 	if err != nil {
 		if err.Error()[0:6] == "no link" {
-			return errors.New(ERR_IpfsWriteToBoard_01)
+			return makeError(ERR_IpfsWriteToBoard_01)
 		}
 		return err
 	}
@@ -163,20 +168,17 @@ func IpfsWriteToBoard(data string, n string) error {
 	for i, v := range nsobj.Links {
 		if v.Name == DIR_IPFS_BOARD {
 			v.Hash = hash
-			//	v.Size = 100
 			nsobj.Links[i] = v
 			found = true
 			break
 		}
 	}
 	if found == false {
-		return errors.New(ERR_IpfsWriteToBoard_02)
+		return makeError(ERR_IpfsWriteToBoard_02)
 	}
 	nwnshash, err := shell.ObjectPut(nsobj)
-	err = putIpnsAdrs(nwnshash)
-	if err != nil {
+	if err = putIpnsAdrs(nwnshash); err != nil {
 		return err
 	}
-
 	return nil
 }
